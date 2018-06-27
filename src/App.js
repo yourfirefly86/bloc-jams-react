d09cc4b6bc2a25c4999e851898b1b224375179b5
Lauren Yeager <Lauren.e.yeager@gmail.com>
2018-06-27 17:42:40 -0400

    Router Checkpoint Work

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <nav>
+          <Link to='/'>Landing</Link>
+          <Link to='/library'>Library</Link>
+        </nav>
+        <h1>Bloc Jams</h1>
+      </header>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
