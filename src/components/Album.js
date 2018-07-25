c75014cb9d6ce70333c6a5fe4100c78ce53c512d
Lauren Yeager <Lauren.e.yeager@gmail.com>
2018-07-25 18:26:57 -0400

    Player Bar: Buttons Checkpoint Work

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -58,6 +59,15 @@ playOrPause(e) {
   this.state.isPlaying ? e.target.className = "ion-md-play" : e.target.className = "ion-md-pause";
 }
 
+handlePrevClick() {
+  const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+  const newIndex = Math.max(0, currentIndex - 1);
+  const newSong = this.state.album.songs[newIndex];
+  this.setSong(newSong);
+  this.play();
+}
+
+
   render() {
     return (
       <section className="album">
@@ -91,6 +101,12 @@ playOrPause(e) {
           <script src="https://unpkg.com/ionicons@4.2.4/dist/ionicons.js"></script>
           </tbody>
         </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+        />
       </section>
     );
   }
